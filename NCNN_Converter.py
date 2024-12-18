93eb0820f6446e0735557c72889e39bc688ff67a
josbor1 <123982835+josbor1@users.noreply.github.com>
2024-12-18 16:13:39 -0600

    Update NCNN_Converter.py
    Changed output resolution to 320 instead of 640.

--- NCNN_Converter.py
+++ NCNN_Converter.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 # Export the model to NCNN format
-model.export(format="ncnn", imgsz=640)  # creates 'yolo11n_ncnn_model'
+model.export(format="ncnn", imgsz=320)  # creates 'yolo11n_ncnn_model'
